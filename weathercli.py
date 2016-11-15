38e1194ba4ec0a936c2ac6028eac8897cb05fec4
Daniel P <0panayi@informatik.uni-hamburg.de>
2016-11-15 06:01:33 +0100

    Changed to use pyowm

--- weathercli.py
+++ weathercli.py
@@ -2,97 +2,23 @@
 # -*- coding: utf-8 -*-
 import argparse
 from collections import defaultdict
-import json
 import os
-import re
 import sys
-import urllib
+import pyowm
 
 from clint.textui import puts, colored
-
 
 SUN = u'\u2600'
 CLOUDS = u'\u2601'
 RAIN = u'\u2602'
 SNOW = u'\u2603'
 
-
-class VerboseFormatter(object):
-
-    def output(self, context):
-        return u"It's {0}\u00B0 and {1}".format(
-            context['temp'],
-            context['conditions'].lower()
-        )
-
-
-class IconifyFormatter(object):
-
-    def output(self, context):
-        return u"{0}\u00B0{1}".format(
-            context['temp'],
-            context['icon']
-        )
-
-
-class WeatherDataError(Exception):
-    pass
-
-
-class OpenWeatherMap(object):
-
-    def __init__(self, formatter=VerboseFormatter()):
-        self.formatter = formatter
-
-    def now(self, query, units='imperial'):
-        raw_data = urllib.urlopen('http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}'.format(
-            urllib.quote_plus(query),
-            units
-        )).read()
-        
-        try:
-            weather = json.loads(raw_data)
-        except ValueError:
-            raise WeatherDataError("Malformed response from weather service")
-
-        context = {}
-        try:
-            context['temp'] = int(weather['main']['temp'])
-            context['conditions'] = weather['weather'][0]['description']
-            context['icon'] = self.icon(weather['weather'][0]['icon'])
-        except KeyError:
-            raise WeatherDataError("No conditions reported for your search")
-
-        return self.formatter.output(context)
-
-    def icon(self, code):
-        codes = defaultdict(int, {
-            '01d': SUN,
-            '01n': SUN,
-            '02d': CLOUDS,
-            '02n': CLOUDS,
-            '03d': CLOUDS,
-            '03n': CLOUDS,
-            '04d': CLOUDS,
-            '04n': CLOUDS,
-            '09d': RAIN,
-            '09n': RAIN,
-            '10d': RAIN,
-            '10n': RAIN,
-            '11d': RAIN,
-            '11n': RAIN,
-            '13d': SNOW,
-            '13n': SNOW,
-        })
-        return codes[code]
-
-
 class Arguments(object):
     QUERY = 'WEATHER'
     UNITS = 'WEATHER_UNITS'
 
     def __init__(self):
-        self.units = defaultdict(lambda: 'imperial', {'celsius': 'metric'})
+        self.units = defaultdict(lambda: 'fahrenheit', {'celsius': 'celsius'})
 
         self.parser = argparse.ArgumentParser(description="Outputs the weather for a given location query string")
         self.parser.add_argument('query', nargs="?", help="A location query string to find weather for")
@@ -110,23 +36,38 @@ class Arguments(object):
     def help(self):
         return self.parser.format_help()
 
+def iconify(w):
+    icon = w.get_weather_icon_name()
+    codes = defaultdict(int, {
+        '01d': SUN,
+        '01n': SUN,
+        '02d': CLOUDS,
+        '02n': CLOUDS,
+        '03d': CLOUDS,
+        '03n': CLOUDS,
+        '04d': CLOUDS,
+        '04n': CLOUDS,
+        '09d': RAIN,
+        '09n': RAIN,
+        '10d': RAIN,
+        '10n': RAIN,
+        '11d': RAIN,
+        '11n': RAIN,
+        '13d': SNOW,
+        '13n': SNOW,
+    })
+    return codes[icon]
 
-def get_temp_color(conditions):
+def get_temp_color(temperature):
     temp_color_map = [
         (40, 'cyan'),
         (60, 'blue'),
         (80, 'yellow')
     ]
-        
-    temperature_re = re.compile('(?P<temperature>-?\d+)')
-    match = temperature_re.search(conditions)
-    if match:
-        for color in temp_color_map:
-            if int(match.group('temperature')) <= color[0]:
-                return color[1]
-        return 'red'
-    return 'white'
-    
+    for color in temp_color_map:
+        if temperature <= color[0]:
+            return color[1]
+    return 'red'
 
 class Weather(object):
 
@@ -135,20 +76,28 @@ class Weather(object):
         arguments = Arguments()
 
         args = arguments.parse(sys.argv[1:], defaults=os.environ)
-        formatter = args['iconify'] and IconifyFormatter() or VerboseFormatter()
-        weather_provider = OpenWeatherMap(formatter=formatter)
+        weather_provider = pyowm.OWM('b388ab985d247a3d54c0e981d0b24a9b') # OpenWeatherMap(formatter=formatter)
 
         if not args['query']:
             print arguments.help()
             sys.exit(1)
 
         try:
-            conditions = weather_provider.now(args['query'], args['units'])
+            observation = weather_provider.weather_at_place(args['query']) # now(args['query'], args['units'])
         except WeatherDataError as e:
             print >> sys.stderr, "ERROR: {0}".format(e.message)
             sys.exit(1)
 
-        puts(getattr(colored, get_temp_color(conditions))(conditions))
+        w = observation.get_weather()
+        temp = w.get_temperature(args['units'])['temp']
+        if args['iconify']:
+            conditions = u"{0}\u00B0 {1}".format(
+            temp,iconify(w))
+        else:
+            conditions = u"It's {0}\u00B0 and {1}".format(
+            temp,w.get_status().lower())
+
+        puts(getattr(colored, get_temp_color(temp))(conditions))
 
 
 if __name__ == '__main__':
